7628e187b7b14a10f964050946305a838bcafbab
carhernandez <carlos.hernandez@convertic.com.co>
2023-01-15 22:45:37 -0500

    version 15/01/2023

--- src/main/java/com/CONVERTICSHOP/demo/modelo/Factura.java
+++ src/main/java/com/CONVERTICSHOP/demo/modelo/Factura.java
@@ -5,36 +5,39 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Table (name = "factura")
+@Table(name = "factura")
 public class Factura {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idFactura;
 
-    @Column (name = "fecha")
+    @Column(name = "fecha")
     private LocalDate fecha;
-
-    private Integer idUsuario;
-
-    private Integer idProducto;
-
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    Usuarios usuarios;
+    @ManyToOne
+    @JoinColumn(name = "id_producto")
+    Productos productos;
+    @Column(name = "cantidad_total")
     private Integer cantidadTotal;
-
+    @Column(name = "iva")
     private Float iva;
-
+    @Column(name = "total")
     private Float total;
 
     public Factura() {
     }
 
-    public Factura(LocalDate fecha, Integer idUsuario, Integer idProducto, Integer cantidadTotal, Float iva, Float total) {
+    public Factura(LocalDate fecha, Usuarios usuarios, Productos productos, Integer cantidadTotal, Float iva, Float total) {
         this.fecha = fecha;
-        this.idUsuario = idUsuario;
-        this.idProducto = idProducto;
+        this.usuarios = usuarios;
+        this.productos = productos;
         this.cantidadTotal = cantidadTotal;
         this.iva = iva;
         this.total = total;
     }
+
 
     public Integer getIdFactura() {
         return idFactura;
@@ -52,20 +55,20 @@ public class Factura {
         this.fecha = fecha;
     }
 
-    public Integer getIdUsuario() {
-        return idUsuario;
+    public Usuarios getUsuarios() {
+        return usuarios;
     }
 
-    public void setIdUsuario(Integer idUsuario) {
-        this.idUsuario = idUsuario;
+    public void setUsuarios(Usuarios usuarios) {
+        this.usuarios = usuarios;
     }
 
-    public Integer getIdProducto() {
-        return idProducto;
+    public Productos getProductos() {
+        return productos;
     }
 
-    public void setIdProducto(Integer idProducto) {
-        this.idProducto = idProducto;
+    public void setProductos(Productos productos) {
+        this.productos = productos;
     }
 
     public Integer getCantidadTotal() {
@@ -97,11 +100,12 @@ public class Factura {
         return "Factura{" +
                 "idFactura=" + idFactura +
                 ", fecha=" + fecha +
-                ", idUsuario=" + idUsuario +
-                ", idProducto=" + idProducto +
+                ", usuarios=" + usuarios +
+                ", productos=" + productos +
                 ", cantidadTotal=" + cantidadTotal +
                 ", iva=" + iva +
                 ", total=" + total +
                 '}';
     }
 }
+
